9f42be2c761f367a37e7e5f5950302a4f1ee18d1
abhirajp075@gmail.com <abhirajp075@gmail.com>
2022-11-29 19:39:03 +0530

    new changes

--- 2.sql
+++ 2.sql
@@ -3,6 +3,12 @@ use company;
 create table employee(SSN varchar(10) primary key,Name varchar(20),Address varchar(30),
 		Sex varchar(10),salary integer,SuperSSN varchar(10),DNo varchar(10));
 create table department(DNo varchar(10) primary key,DName varchar(20),MgrSSN varchar(10),MgrStartDate varchar(10));
-create table DLocation(DNo varchar(10),DLoc varchar(20) primary key);
+
+# Down query is to be checked after that to be run because DlocID is the primary key new to be added
+# The new DlocID primary key is to be added becoz in a multi valued attribute the structure is to be
+# new primary key ,the primary key of the parent entity,the values
+
+create table DLocation(DlocID varchar(10) primary key,DNo varchar(10),DLoc varchar(20));
+
 create table project(PNo varchar(10) primary key,PName varchar(20),PLocation varchar(20),DNo varchar(10));
 create table works_On(SSN varchar(10),PNo varchar(10),hours integer);
